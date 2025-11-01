5e766f3d04e359b002dfa68aa142d0391884536c
Rajma Chaawal! <85783946+RajmaChaawal@users.noreply.github.com>
2025-11-01 02:50:33 +0000

    added working of JS in theoritical context!

---
+++ JavaScript--main/00_origin/02_what-is-JS.js
@@ -0,0 +1,28 @@
+//Lecture 2 => What really is JS?
+// JAVASCRIPT is a : -
+//      ~ High Level -> Highly Portable and very close to human language!
+//      ~ Prototype based Obj-Oriented -> Object Oriented Programming!
+//      ~ MultiiParadigm -> multiple approaches for programming!
+//      ~ Interpreted / Just-in-Time Compiled -> Compiled and then Executed line-by-line!
+//      ~ Dynamic -> datatype can change at runtime and not fixed!
+//      ~ Single Threaded -> cannot execute multiple operations at a once!
+//      ~ Garbade Collected -> automatically manages memory allocation and deallocation
+//                  - programming language with : -
+//                      ~ first class functinos -> functions that can be assigned to a variable and  can be passed to other functions!
+//                      ~ non-blocking event-loop concurrency model!
+
+
+// -> Originally meant to be used by browsers!
+// -> Today, its everywhere :-
+//  ~ Client-Side (Browser)
+//  ~ Server Side (Node.js)
+//  ~ Desktop Apps (Electron.js)
+//  ~ Mobile Apps (React Native, Ionic)
+//  ~ Games & Graphics (Canvas, WebGL, Three.js)
+//  ~ AI & ML (TensorFlow.js)
+
+// 1 - "Let's see how JS actually works" :
+// -> JS is an engine standardized by  ES2024, that every browser follows!
+// -> JS engine contains a CallStack and Heap!
+// -> The Callstack is responsible for code executes with execution context!
+// -> Heap is responsible for String objects in an unstructured memory pool, that are necessit for our application!
