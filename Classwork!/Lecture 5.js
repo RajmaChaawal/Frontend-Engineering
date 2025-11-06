4a6ec27da687975f6175bd5bde827b899efec39a
RajmaChaawal <luckysharma0729@gmail.com>
2025-11-06 14:05:54 +0530

    .added dom manipulation

--- Classwork!/Lecture 5.js
+++
@@ -1,9 +0,0 @@
-// Lecture 5:  DOM Manipulation!
-
-const listItems = document.getElementsByClassName("list-items");
-
-console.log(listItems); 
-
-listItems[2].innerText = "Byeee"
-
-// Difference between .textContent, .innerHTML, .innerText!
